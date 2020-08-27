058df11f915cc743964f3ad3a2f6bcf6ac9785d6
Johnson Liu <jyliu17@gmail.com>
2020-08-27 23:49:36 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -44,7 +44,7 @@ def matrix_lookup(matrix, row, column)
   # Return the matrix's content at that row and and column
   
   matrix_lookup = [
-    matrix[1][1]
+    assembled_matrix[1][1]
     ]
 end
 
